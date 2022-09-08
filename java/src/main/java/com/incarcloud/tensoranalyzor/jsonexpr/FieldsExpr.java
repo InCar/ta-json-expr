9d25420cd2df713414e32ba0d7f09746a056c8d7
AceBear <AceBear@users.noreply.github.com>
2022-09-08 16:46:59 +0800

    json schema validation (#2)
    * schema for fields
    
    * json validation
    
    * json validation code

---
+++ java/src/main/java/com/incarcloud/tensoranalyzor/jsonexpr/FieldsExpr.java
@@ -0,0 +1,35 @@
+package com.incarcloud.tensoranalyzor.jsonexpr;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.slf4j.*;
+
+public class FieldsExpr{
+    private static final Logger s_logger = LoggerFactory.getLogger(FieldsExpr.class);
+
+    /**
+     * 验证json是否符合schema
+     */
+    public boolean Validate(String json){
+        try(InputStream schemaStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("./schema-fields.json")) {
+            JSONObject rawSchema = new JSONObject(new JSONTokener(schemaStream));
+            Schema schema = SchemaLoader.load(rawSchema);
+            schema.validate(new JSONObject(json));
+        }
+        catch(IOException ex) {
+            s_logger.warn("Validate json failed\n\t{}", ex.toString());
+            return false;
+        }
+        catch(ValidationException ex){
+            s_logger.warn("Validate json failed\n\t{}", ex.toString());
+            return false;
+        }
+        return true;
+    }
+}
